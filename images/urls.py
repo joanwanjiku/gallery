367d75580edac38d3e9ed319360630d5b47a7259
Joan_E <joanevans18@gmail.com>
2020-08-04 11:02:04 +0300

    search and location urls

--- images/urls.py
+++ images/urls.py
@@ -4,7 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='welcome')
+    path('', views.index, name='welcome'),
+    path('search/', views.image_results, name='image_results'),
+    path('location/<int:loc_id>', views.image_location, name='image_location')
 ]
 
 if settings.DEBUG:
